e8a9688e67de686dbd0a36292a0913a8ac25736f
RaresPRT <raresgsmart@gmail.com>
2020-08-06 21:46:12 +0300

    Shared the project to git

---
+++ Yapal-Papal-Chess-Engine/src/ro/raresprt/chess/MainRunner.java
@@ -0,0 +1,32 @@
+package ro.raresprt.chess;
+
+public class MainRunner {
+	
+	private static void init() {
+		
+	}
+	
+	private static void term() {
+		
+	}
+	
+	private static void runEngine() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			init();
+			
+			runEngine();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			term();
+		}
+	}
+
+}
